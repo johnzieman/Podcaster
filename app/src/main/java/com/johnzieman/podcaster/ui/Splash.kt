fb83500b6503b042a8259ed30401376545afec8b
mi4 <j.g.zokirov@gmail.com>
2022-07-12 13:22:03 +0500

    -upd

---
+++ app/src/main/java/com/johnzieman/podcaster/ui/Splash.kt
@@ -0,0 +1,20 @@
+package com.johnzieman.podcaster.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.johnzieman.podcaster.R
+
+
+class Splash : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
