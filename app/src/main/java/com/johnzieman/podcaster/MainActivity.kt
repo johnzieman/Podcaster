63eabc374d6e7f229a6a4bacf936c80bc8ded887
mi4 <j.g.zokirov@gmail.com>
2022-07-21 19:48:21 +0500

    -upd

--- app/src/main/java/com/johnzieman/podcaster/MainActivity.kt
+++ app/src/main/java/com/johnzieman/podcaster/MainActivity.kt
@@ -2,10 +2,21 @@ package com.johnzieman.podcaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.johnzieman.podcaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.nav_host)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/johnzieman/podcaster/ui/SavedEpisodes.kt
@@ -0,0 +1,21 @@
+package com.johnzieman.podcaster.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.johnzieman.podcaster.R
+
+
+class SavedEpisodes : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_episodes, container, false)
+    }
+
+}
